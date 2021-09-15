3ec9d3b4c9054cf6f053a90ef6ef498b80e23367
Jeffrey Stiles <freb@users.noreply.github.com>
2021-09-15 09:45:30 -0700

    initial commit

---
+++ src/pages/example/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import base64Logo from "./_components/assets/logo.js";
+  import Button from "@smui/button"
+</script>
+
+<!-- routify:option name="example-app" -->
+
+<Button>TEST</Button>
+
+<div style="width: 100%; text-align: center; margin-top: 4rem;">
+  <img src="data:image/png;base64, {base64Logo}" alt="logo" style="max-width: 100%; padding-bottom: 128px" />
+  <div>
+    <b>Guide:</b>
+    <br />
+    <a href="https://routify.dev">https://routify.dev</a>
+  </div>
+
+  <br />
+  <div>
+    <b>This template:</b>
+    <br />
+    <a href="https://github.com/roxiness/routify-starter">
+      https://github.com/roxiness/routify-starter
+    </a>
+  </div>
+
+</div>
